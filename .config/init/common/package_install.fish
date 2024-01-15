0163a90536c2363bde6be88f4c0b0124bb699657
alexpfx <alexpfba@gmail.com>
2024-01-15 15:34:43 -0300

    vpnscript

--- .config/init/common/package_install.fish
+++ .config/init/common/package_install.fish
@@ -4,7 +4,7 @@ set -l network chromium openssh nmap openconnect
 set -l dev go git github-cli vscodium-bin vscodium-bin-marketplace insomnium-bin docker docker-compose docker-buildx python-pip python-pipx meld python
 
 set -l fonts ttf-font-awesome ttf-jetbrains-mono 
-set -l token opensc pkcs11-helper pcsc-tools ca-certificates-icp_br safesignidentityclient sac-core insomnium-bin
+set -l token opensc pkcs11-helper pcsc-tools ca-certificates-icp_br safesignidentityclient sac-core insomnium-bin h-m-m-git
 
 set -l pacotes $tools $fonts $dev $token $network
 
--- .config/init/scripts/vpn.sh
+++ .config/init/scripts/vpn.sh
@@ -6,5 +6,23 @@ group=$(cat /data/params/group)
 crt=$(cat /data/params/crt)
 server_crt=$(cat /data/params/server_crt)
 
-echo "sudo openconnect --no-dtls --authgroup=$group --protocol=gp -u $user -c $crt -v $gtw"
+echo -v "sudo openconnect --no-dtls --authgroup=$group --protocol=gp -u $user -c $crt -v $gtw"
+
+# Armazena o tempo inicial
+start_time=$SECONDS
+
+# Função para calcular e exibir o tempo decorrido
+show_elapsed_time() {
+  while true; do
+    elapsed_time=$((SECONDS - start_time))
+    echo "Tempo decorrido: $elapsed_time segundos\n"
+    sleep 30
+  done
+}
+
+show_elapsed_time &
+
 sudo openconnect --no-dtls --authgroup=$group --protocol=gp -u $user -c $crt -v $gtw
+
+
+kill $!
