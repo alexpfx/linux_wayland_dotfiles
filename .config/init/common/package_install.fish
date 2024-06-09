02a866b48a615356cf10538580574b91facd0553
alexpfx <alexpfba@gmail.com>
2024-06-09 00:15:46 -0300

    changs

--- .config/init/common/package_install.fish
+++ .config/init/common/package_install.fish
@@ -6,8 +6,7 @@ set -l dev go git github-cli vscodium-bin vscodium-bin-marketplace insomnium-bin
 set -l fonts ttf-font-awesome ttf-jetbrains-mono 
 set -l token opensc pkcs11-helper pcsc-tools ca-certificates-icp_br safesignidentityclient sac-core insomnium-bin h-m-m-git
 
-set -l emuladores retroarch-assets-ozone retroarch retroarch-assets-ozone retroarch-assets-xmb retroarch-assets-glui
-set -l ventoy-bin
+set -l emuladores retroarch-assets-ozone retroarch retroarch-assets-ozone retroarch-assets-xmb retroarch-assets-glui  ventoy-bin dropbox
 
 
 set -l pacotes $tools $fonts $dev $token $network $emuladores
